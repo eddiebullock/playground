cc0808d404208d035d67c1443c3b3251c1d08a53
Edward Bullock <eb2007@cam.ac.uk>
2025-07-12 13:02:06 +0100

    target colum created

--- bullpy/c4_play2/src/data_loader.py
+++ bullpy/c4_play2/src/data_loader.py
@@ -4,12 +4,10 @@ import pandas as pd
 
 def load_data(file_path):
     """load csv file into pandas dataframe"""
-    return pd.read_csv(filepath)
+    return pd.read_csv(file_path)
 
 if __name__ == "__main__":
     df = load_data("/Users/eb2007/Library/CloudStorage/OneDrive-UniversityofCambridge/Documents/PhD/data/data_c4_raw.csv")
     print(df.info())
     print(df.head())
 
-    ### small test
-    
